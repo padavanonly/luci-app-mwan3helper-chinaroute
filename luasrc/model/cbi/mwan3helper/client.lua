04ec2c6aaa0dbf11e819e38b4615db93eb8707d2
padavanonly <padavanonly@github.com>
2023-12-16 15:18:32 +0800

    ip route command select dev

--- luasrc/model/cbi/mwan3helper/client.lua
+++ luasrc/model/cbi/mwan3helper/client.lua
@@ -15,7 +15,8 @@ o = s:option(ListValue, "mode", translate("转发流量模式"))
 o:value("0", translate("国外流量"))
 o:value("1", translate("GFWLIST流量"))
 
-o = s:option(Value, "dns", translate("转发流量至"))
+o = s:option(Value, "dns", translate("转发流量至IP"))
+o = s:option(Value, "dev", translate("设备"))
 o = s:option(Flag, "dnsen", translate("启用 DNS 防污染"))
 o.rmempty = false
 o = s:option(Value, "dnsserver", translate("Anti-pollution DNS Server"))
